c1337826dde701879883c4985d31d420aeab3fa2
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-04-24 14:42:54 +0000

    added a setup function

--- power_current_monitor.ino
+++ power_current_monitor.ino
@@ -9,3 +9,18 @@ const int relayPin = 7;
 float R = 220.0;
 float supplyVoltage = 5.0;
 float thresholdMW = 50.0;
+
+void setup() {
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("System Booting");
+  lcd.setCursor(0, 1);
+  lcd.print("Checking LCD...");
+  delay(2000);
+  lcd.clear();
+}
